e79eaed7c9362d30ee0b380a24ac4bd3b674d64f
GIOlitigation <40328999+GIOlitigation@users.noreply.github.com>
2018-06-17 13:15:44 +1000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+function addPeriods(){
+	//used to dynamically allow the user to enter in multiple periods with different weekly amounts
+	var amount = document.getElementById("payPeriods").value;
+	var container = document.getElementById("multiContainer");
+
+	while (container.hasChildNodes()) {
+                container.removeChild(container.lastChild);
+            }
+            for (i=0;i<amount;i++){
+                // Append a node with text and line breaks
+                container.appendChild(document.createTextNode("Pay Period " + (i+1)));
+                container.appendChild(document.createElement("br"));
+				container.appendChild(document.createTextNode("Date From:"));
+                container.appendChild(document.createElement("br"));
+                // Create an <input> element, set its type and name attributes
+                var inputFrom = document.createElement("input");
+                inputFrom.type = "date";
+                inputFrom.name = "dateFrom" + i;
+                container.appendChild(inputFrom);
+                // Append a line break 
+                container.appendChild(document.createElement("br"));
+				container.appendChild(document.createTextNode("Date To:"));
+                container.appendChild(document.createElement("br"));
+                // Create an <input> element, set its type and name attributes
+                var inputTo = document.createElement("input");
+                inputTo.type = "date";
+                inputTo.name = "dateTo" + i;
+                container.appendChild(inputTo);
+                // Append a line break 
+                container.appendChild(document.createElement("br"));
+				container.appendChild(document.createTextNode("Weekly Amount:"));
+                container.appendChild(document.createElement("br"));
+                // Create an <input> element, set its type and name attributes
+                var inputAmount = document.createElement("input");
+                inputAmount.type = "text";
+                inputAmount.name = "weeklyAmount" + i;
+                container.appendChild(inputAmount);
+                // Append a line break 
+                container.appendChild(document.createElement("br"));
+                container.appendChild(document.createElement("br"));
+            }
+}
